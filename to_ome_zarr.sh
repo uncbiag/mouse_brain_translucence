d91492060780a5ce63a6cc1b3c3e9fc66e1d39c5
Matt McCormick <matt.mccormick@kitware.com>
2024-04-19 11:14:52 -0400

    Add spacing, units to OME-Zarr generation

--- to_ome_zarr.sh
+++ to_ome_zarr.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-# Copy this script to data directory containing the *.tif files.
-
 for ff in *.tif; do
   echo "Converting $ff"
-  ngff-zarr -i $ff -o $(basename $ff .tif).ome.zarr --input-backend itk -m dask_image_gaussian
+  # For 12x downsample, 1.8x1.8x5.5 um x y z spacing
+  ngff-zarr -i $ff -o $(basename $ff .tif).ome.zarr \
+    --input-backend itk \
+    -m dask_image_gaussian \
+    -s z 66.0 y 21.6 x 21.6 \
+    -u z micrometer y micrometer x micrometer
 done
